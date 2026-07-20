4092652fd50ee95435f3b46d582262ee82c24c90
pranavnargale23-pixel <pranavnargale23@gmail.com>
2026-07-20 15:52:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'voucher-maker-v5';
+const CACHE = 'voucher-maker-v6';
 const FILES = ['./','index.html','style.css','app.js','manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
